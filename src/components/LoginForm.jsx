c3c96a586ff7e00d532054191ed94fd94168a6fe
MateusCastelan <mateus_castelanp@hotmail.com>
2023-11-20 15:37:31 -0300

    implementaçao inicial login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import styles from '../styles/LoginForm.module.css'
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 export const LoginForm = () => {
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8080/api/users/login', {
+        username,
+        password,
+      },
+        {
+          withCredentials: true,
+        });
+
+      if (response.status === 200) {
+        router.push('/editUser');
+      } else {
+        console.error('Credenciais inválidas', response.data);
+      }
+    } catch (err) {
+      console.error('Erro ao fazer login:', err.message);
+    }
   };
 
   return (
@@ -12,12 +37,12 @@ export const LoginForm = () => {
       <form action="/login" method="post" onSubmit={handleSubmit}>
         <h1>Login</h1>
         <section className={styles.inputBox}>
-          <input type="text" name="username" required />
+          <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} required />
           <label>Usuário</label>
           <i className='bx bxs-user'></i>
         </section>
         <section className={styles.inputBox}>
-          <input type="password" name="password" required />
+          <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} required />
           <label>Senha</label>
           <i className='bx bxs-lock-alt'></i>
         </section>
